a66895b7882b0c9c9d8fc28a700502661d56fcd2
Joseph Chen <josephchen@CNSHBL0418.local>
2019-11-12 14:38:24 +0800

    Added easy ecs-fargate ApplicationLoadBalancedFargateService.

--- bin/cdk-ecs-fargate-us-west.ts
+++ bin/cdk-ecs-fargate-us-west.ts
@@ -3,5 +3,9 @@ import 'source-map-support/register';
 import cdk = require('@aws-cdk/core');
 import { CdkEcsFargateUsWestStack } from '../lib/cdk-ecs-fargate-us-west-stack';
 
+const env = {
+    region: 'us-west-2',
+    account: '374801192098'
+};
 const app = new cdk.App();
-new CdkEcsFargateUsWestStack(app, 'CdkEcsFargateUsWestStack');
+new CdkEcsFargateUsWestStack(app, 'CdkEcsFargateUsWestStack', { env });
--- lib/cdk-ecs-fargate-us-west-stack.ts
+++ lib/cdk-ecs-fargate-us-west-stack.ts
@@ -1,9 +1,26 @@
 import cdk = require('@aws-cdk/core');
+import { Vpc } from '@aws-cdk/aws-ec2';
+import { Cluster, ContainerImage, FargateTaskDefinition } from '@aws-cdk/aws-ecs';
+import { ApplicationLoadBalancedFargateService } from '@aws-cdk/aws-ecs-patterns';
 
 export class CdkEcsFargateUsWestStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+    const vpc = Vpc.fromLookup(this, 'defaultstack/defaultvpc', {
+      isDefault: false
+    })
+
+    const cluster = new Cluster(this,  'ecsCluster', {
+      vpc
+    })
+
+    new ApplicationLoadBalancedFargateService(this, "MyFargateService", {
+      cluster: cluster,
+      taskImageOptions: { image: ContainerImage.fromRegistry("amazon/amazon-ecs-sample") },
+
+    });
+
   }
 }
